ca46feca5ef36e2d3e962548f56c3a33a311b634
84Ebi <ebrahimkhosravani3@gmail.com>
2025-10-02 16:23:47 +0330

    Refactor next.config.ts to use consistent path imports for outputFileTracingRoot

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,14 @@
 import type { NextConfig } from "next";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  outputFileTracingRoot: require('path').join(__dirname, '../../'),
+  outputFileTracingRoot: path.join(__dirname, '../../'),
   experimental: {
     // appDir: true,
   },
