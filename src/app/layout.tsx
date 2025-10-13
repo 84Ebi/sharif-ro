d875810b51f48c69fd72ed263e000c6fb9b1f561
84Ebi <ebrahimkhosravani3@gmail.com>
2025-10-13 16:54:31 +0330

    mamad rezvani kerm dasht

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import './globals.css'
 import Body from '../components/Body'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Food Hall Delivery',
@@ -17,7 +14,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Body className={inter.className}>{children}</Body>
+      <Body className="">{children}</Body>
     </html>
   )
 }
